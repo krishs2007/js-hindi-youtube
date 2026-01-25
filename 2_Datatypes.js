2a618a8e522561322021e6c4b61ff75eb6cd1c14
Krish Sonani <krishsonani2007@gmail.com>
2026-01-25 18:10:33 +0530

    Add files via upload

---
+++ 2_Datatypes.js
@@ -0,0 +1,4 @@
+"Use Strict"; // treat all JS Code as newer Version
+
+// alert("Hello") // We Are Using nodejs, not Browser
+console.log(3+3); console.log("Krish") // Code Readability Should be High
