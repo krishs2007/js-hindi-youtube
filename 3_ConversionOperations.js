24f45932117fe3ce733d020d4f7aaaad640d9603
Krish Sonani <krishsonani2007@gmail.com>
2026-01-26 20:28:40 +0530

    Add files via upload

---
+++ 3_ConversionOperations.js
@@ -0,0 +1,19 @@
+let score="33abc"
+let score1=null
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+let v2=Number(score1);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+console.log(v2)
+let isLoggedIn=1
+let boolenIsLoggedIn=Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn)
+// 0=>true : 1=>false
+// ""=>false
+let someNumber=33
+let s2n= String(someNumber)
+console.log(s2n)
+console.log(typeof s2n)
